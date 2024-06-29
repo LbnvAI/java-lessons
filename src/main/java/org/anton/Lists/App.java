d3f43f16f2f59c179196495559f1bbc87fc14005
Anton Lobanov <ailobanov97@mail.ru>
2024-06-29 20:11:42 +0300

    Use for-each to seek items in List<Integer>

--- src/main/java/org/anton/Lists/App.java
+++ src/main/java/org/anton/Lists/App.java
@@ -1,10 +1,22 @@
 package org.anton.Lists;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class App {
+
     public static String getOrDefault(List<String> collection, int index, String defaultValue) {
         if (index >= collection.size() || index < 0) return defaultValue;
         else return collection.get(index);
+    }
+
+    // Seek items by for-each
+    public static List<Integer> replaceByZero(List<Integer> numbers) {
+        List<Integer> noNegativenumbersList = new ArrayList<Integer>();
+        for (Integer num : numbers) {
+            if (num < 0) noNegativenumbersList.add(0);
+            else noNegativenumbersList.add(num);
+        }
+        return noNegativenumbersList;
     }
 }
