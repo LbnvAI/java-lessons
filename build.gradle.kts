0ece99f4094003f3f706f911620122fd919a10d4
Anton Lobanov <ailobanov97@mail.ru>
2024-06-29 17:49:45 +0300

    Create Gradle Project Connect Lombok

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+    id("io.freefair.lombok") version "8.4"
+}
+
+group = "org.anton"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
