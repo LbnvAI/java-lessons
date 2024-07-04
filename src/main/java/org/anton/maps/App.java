92cf642d5837d2117d13199d37d75afba9e943ce
Anton Lobanov <ailobanov97@mail.ru>
2024-07-04 12:47:31 +0300

    Find the most populated city

--- src/main/java/org/anton/maps/App.java
+++ src/main/java/org/anton/maps/App.java
@@ -1,6 +1,7 @@
 package org.anton.maps;
 
 import java.util.Map;
+import java.util.Set;
 
 public class App {
     private static final Map<String, Double> products = Data.getProducts();
@@ -11,5 +12,19 @@ public class App {
             if (!discountCart.containsKey(productName)) return products.get(productName);
             else return products.get(productName) * discountCart.get(productName) / 100;
         } else return null;
+    }
+
+    // Find the most populated city
+    public static String getMostPopulatedCity(Map<String, Integer> cities) {
+        Set<String> cityNames = cities.keySet();
+        String mostPopulatedCity = null;
+        int maxValue = 0;
+        for (String cityName : cityNames) {
+            if (cities.get(cityName) > maxValue) {
+                maxValue = cities.get(cityName);
+                mostPopulatedCity = cityName;
+            }
+        }
+        return mostPopulatedCity;
     }
 }
