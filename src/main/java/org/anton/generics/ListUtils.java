76b2fba33630558184148a72b1caababe326d43b
Anton Lobanov <ailobanov97@mail.ru>
2024-07-09 13:52:46 +0300

    Create static generic method

--- src/main/java/org/anton/generics/ListUtils.java
+++ src/main/java/org/anton/generics/ListUtils.java
@@ -2,6 +2,7 @@ package org.anton.generics;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class ListUtils<T> {
 
@@ -10,5 +11,16 @@ public class ListUtils<T> {
         result.addAll(l1);
         result.addAll(l2);
         return result;
+    }
+
+    // Create static generic method
+    public static <T> List<T> filter(List<T> list, Predicate<T> p) {
+        List<T> filtredList = new ArrayList<>();
+        for (T t : list) {
+            if (p.test(t)) {
+                filtredList.add(t);
+            }
+        }
+        return filtredList;
     }
 }
