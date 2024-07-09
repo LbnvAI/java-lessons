00d7fd3110707b5f2ec2fcc702c21ed2a174535e
Anton Lobanov <ailobanov97@mail.ru>
2024-07-09 21:00:03 +0300

    User collect and Collectors

--- src/main/java/org/anton/Main.java
+++ src/main/java/org/anton/Main.java
@@ -7,16 +7,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        var products = List.of(
-                new Product("bread", "grocery", 10),
-                new Product("milk", "grocery", 15),
-                new Product("harry potter", "books", 500),
-                new Product("mobile phone", "electronics", 10000),
-                new Product("bmw", "transport", 2000000),
-                new Product("computer", "electronics", 20000),
-                new Product("cheese", "grocery", 200),
-                new Product("notebook", "electronics", 25000)
-        );
-        System.out.println(App.getTotalPrice(products));
+
     }
 }
--- src/main/java/org/anton/streams/App.java
+++ src/main/java/org/anton/streams/App.java
@@ -2,8 +2,18 @@ package org.anton.streams;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class App {
+    private static final List<String> FREE_DOMAINS = List.of(
+            "gmail.com",
+            "yandex.ru",
+            "hotmail.com",
+            "yahoo.com"
+    );
+
     // Try to use streams
     public static List<String> sortWords(List<String> words) {
         return words.stream().sorted().toList();
@@ -32,5 +42,17 @@ public class App {
         return goods.stream()
                 .filter((good) -> good.getCategory().equals("electronics"))
                 .reduce(0, (acc, good) -> acc + good.getPrice(), Integer::sum);
+    }
+
+    // User collect and Collectors
+    public static Map<String, Long> getFreeDomainsCount(List<String> emails) {
+        return emails.stream()
+                .filter(mail -> {
+                    var domain = mail.split("@")[1];
+                    return FREE_DOMAINS.contains(domain);
+                })
+                .collect(Collectors.groupingBy(mail -> {
+                    return mail.split("@")[1];
+                }, Collectors.counting()));
     }
 }
